4d2991f4cbb6746c0de011cc11f135a0294f4fa9
Ahmed Bawazer <40899021+ahmeddeveloper55@users.noreply.github.com>
2022-07-15 05:10:50 +0530

    Add files via upload

---
+++ routesData.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const { modelGlutenAdditive } = require('../modules/modelData');
+
+const ObjectId = require('mongoose').Types.ObjectId;
+//Get All Items
+router.get('/api/data', (req, res) => {
+    modelGlutenAdditive.find({}, (err, data) => {
+        if (!err) {
+            res.send(data);
+            console.log(data)
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+// Get Single Item (First Way)
+
+router.get('/api/employee/:id', (req, res) => {
+    modelGlutenAdditive.findById(req.params.id, (err, data) => {
+        if (!err) {
+            res.send(data);
+        } else {
+            console.log(err);
+        }
+    });
+});
+// save single Itme
+router.post('/api/data/add', (req, res) => {
+    const glutenitem = new modelGlutenAdditive({
+        number: req.body.number,
+        description: req.body.description,
+        category: req.body.category,
+        status: req.body.status
+
+    });
+    glutenitem.save((err, data) => {
+        if (!err) {
+            res.status(200).json({ code: 200, message: 'Item Added Successfully', addItem: data })
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+// Update Item
+
+router.put('/api/data/update/:id', (req, res) => {
+
+
+    const Item1 = {
+        number: req.body.number,
+        description: req.body.description,
+        category: req.body.description,
+        status: req.body.description
+    };
+    modelGlutenAdditive.findByIdAndUpdate(req.params.id, { $set: Item1 }, { new: true }, (err, data) => {
+        if (!err) {
+            res.status(200).json({ code: 200, message: 'Item Updated Successfully', updateItem: data })
+        } else {
+            console.log(err);
+        }
+    });
+});
+// Delete Item
+router.delete('/api/data/:id', (req, res) => {
+
+    modelGlutenAdditive.findByIdAndRemove(req.params.id, (err, data) => {
+        if (!err) {
+            // res.send(data);
+            res.status(200).json({ code: 200, message: 'Item deleted', deleteItem: data })
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+module.exports = router
